0439144869b07c97674fca95f68d5d01c592906e
Vkutti <70446320+Vkutti@users.noreply.github.com>
2020-09-05 09:52:27 -0700

    multiply part

--- test_Multiply.py
+++ test_Multiply.py
@@ -1,23 +1,16 @@
-
 import pytest
 
-from MultiplayFunctions import multiplying
-
-billy = 5
-bob = 10
-joe = 15
-ven = 20
-kat = 25
+from multiply import multiply
 
 
 @pytest.mark.parametrize("a, b, expected", [
     (1, 2, 2),
     (8000600, 6405006, 51243891003600),
-    (billy, bob, 50),
-    (billy, 5, 25),
-    (ven, kat, 500),
-
-
+    (-1, -2, 2),
+    (-50, 2, -100),
+    (-2**5, -9**9, 12397455648),
+    (2, 0, 0),
+    (100**9, 150**5, 75937500000000000000000000000),
 ])
-def test_add(a, b, expected):
-    assert multiplying(a, b) == expected
+def test_multiply(a, b, expected):
+    assert multiply(a, b) == expected
