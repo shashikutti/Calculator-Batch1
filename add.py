5ed3a5500074dfb18ca8ff102eeebacfbdcf5129
pavitravivek <pavitravivek6215@gmail.com>
2020-09-09 19:54:40 -0700

    Pavitra(divide).

--- add.py
+++ add.py
@@ -19,3 +19,22 @@ def add(a, b):
 def check_type(acceptable_types, type, foo):
     if type not in acceptable_types:
         raise TypeError(f'{foo} input parameter must be an int. Instead it is {str(type)}')
+
+
+def divide(a, b):
+    type1 = type(a)
+    type2 = type(b)
+
+    if a is None:
+        return b
+
+    if b is None:
+        return a
+
+    if type1 is not int:
+        raise TypeError('First input parameter must be an int. Instead it is ' + str(type1))
+
+    if type2 is not int:
+        raise TypeError('Second input parameter must be an int. Instead it is ' + str(type2))
+
+    return a / b
--- test_divide.py
+++ test_divide.py
@@ -1,25 +1,6 @@
 import pytest
 
 
-def divide(a, b):
-    type1 = type(a)
-    type2 = type(b)
-
-    if a is None:
-        return b
-
-    if b is None:
-        return a
-
-    if type1 is not int:
-        raise TypeError('First input parameter must be an int. Instead it is ' + str(type1))
-
-    if type2 is not int:
-        raise TypeError('Second input parameter must be an int. Instead it is ' + str(type2))
-
-    return a / b
-
-
 @pytest.mark.parametrize("a, b, expected", [
     (2, 1, 2),
     (3600, 400, 9),
