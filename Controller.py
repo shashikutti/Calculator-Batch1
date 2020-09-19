97de633c157128ca1d2d1f186ab22ff2dca9bef3
Vkutti <70446320+Vkutti@users.noreply.github.com>
2020-09-19 09:05:42 -0700

    Made num3

--- Controller.py
+++ Controller.py
@@ -10,25 +10,25 @@ def main():
     subtract = '-'
     multiply = '×'
     divide = '÷'
-    squared = '²'
+    exponent = '**'
     result = 0
     if request.method == 'POST':
         display_text = request.form['display']
         if add in display_text:
-            num1, num2 = display_text.split(add)
-            result = calc_model.add(num1, num2)
+            num1, num2, num3= display_text.split(add)
+            result = calc_model.add(num1, num2, num3)
         elif subtract in display_text:
-            num1, num2 = display_text.split(subtract)
-            result = calc_model.subtract(num1, num2)
+            num1, num2, num3 = display_text.split(subtract)
+            result = calc_model.subtract(num1, num2, num3)
         elif multiply in display_text:
-            num1, num2 = display_text.split(multiply)
-            result = calc_model.multiply(num1, num2)
+            num1, num2, num3 = display_text.split(multiply)
+            result = calc_model.multiply(num1, num2, num3)
         elif divide in display_text:
-            num1, num2 = display_text.split(divide)
-            result = calc_model.divide(num1, num2)
-        elif squared in display_text:
-            num1, num2 = display_text.split(squared)
-            result = calc_model.squared(num1)
+            num1, num2, num3 = display_text.split(divide)
+            result = calc_model.divide(num1, num2, num3)
+        elif exponent in display_text:
+            num1, num2, num3 = display_text.split(exponent)
+            result = calc_model.exponent(num1, num2, num3)
 
         return render_template('Calc_View.html', result=result)
     else:
--- calc_model.py
+++ calc_model.py
@@ -1,28 +1,36 @@
-def add(num1, num2):
+def add(num1, num2, num3):
     num1 = float(num1)
     num2 = float(num2)
+    num3 = float(num3)
     return num1 + num2
 
 
-def multiply(num1, num2):
+def multiply(num1, num2, num3):
     num1 = float(num1)
     num2 = float(num2)
+    num3 = float(num3)
     return num1 * num2
 
 
-def subtract(num1, num2):
+def subtract(num1, num2, num3):
     num1 = float(num1)
     num2 = float(num2)
+    num3 = float(num3)
     return num1 - num2
 
 
-def divide(num1, num2):
+def divide(num1, num2, num3):
     num1 = float(num1)
     num2 = float(num2)
+    num3 = float(num3)
     return num1 / num2
 
 
-def squared(num1):
+def exponent(num1, num2, num3):
     num1 = float(num1)
-    return num1 * num1
+    num2 = float(num2)
+    num3 = float(num3)
+    return num1 ** num2
+
+
 
